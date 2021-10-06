09cb6e24ab9b6ff6a776875badb9ce2e3e244dd0
wansik5991 <kevin103@daum.net>
2021-10-06 23:53:15 +0900

    Programmers_Link_Island
    프로그래머스_섬연결하기

---
+++ Linking_Island.py
@@ -0,0 +1,11 @@
+import numpy as np
+import os
+os.system('cls')
+
+def solution(n, costs):
+    island = np.identity(n)
+    print(island)
+    answer = 0
+    return answer
+
+print(solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]))
